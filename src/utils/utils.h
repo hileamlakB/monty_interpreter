c0fe3cba36ca9a29586bca540cfdcb5f3b5e874f
hileamlakb <hilea.buisness@gmail.com>
2020-12-20 00:02:20 +0300

    Add utils

---
+++ src/utils/utils.h
@@ -0,0 +1,15 @@
+#ifndef UTILS
+#define UTILS
+
+/*Functions inside utiils folder*/
+
+
+/*Memory utils*/
+void freedp(char **);
+int arlen(char **);
+void trims(char **, char *);
+void *smalloc(unsigned int);
+void *srealloc(void *, unsigned int);
+
+
+#endif /*UTILS*/
